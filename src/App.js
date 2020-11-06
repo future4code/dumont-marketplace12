c4e61ed54b087fa4467c2a2c35ea7d7d73734cbb
SarahHessel <sarah.phessel@gmail.com>
2020-11-06 12:22:46 -0300

    App.js limpo

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {CadastroUsuario} from './components/CadastroUsuario'
-
-class App extends React.Component {
-    render() {
-        return <CadastroUsuario/>
-    }
-}
-export default App
